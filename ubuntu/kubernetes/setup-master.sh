d8c8c72d6123f91a1016af09cce53818934faab6
Ray Fabian <rayfabian@Reys-MacBook-Pro.local>
2018-06-17 23:16:20 +1200

    update readme

--- ubuntu/kubernetes/setup-master.sh
+++ ubuntu/kubernetes/setup-master.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #get IP address
 masterIP=`ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`
-echo "Master IP address"
-echo "$masterIP"
+echo "Master IP address : $masterIP"
