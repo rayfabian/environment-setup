d2c942e9ba5088d1e464b71b03e6352fc2393f8b
Ray Fabian <rayfabian@Reys-MacBook-Pro.local>
2018-06-17 23:44:57 +1200

    update readme

--- ubuntu/kubernetes/setup-master.sh
+++ ubuntu/kubernetes/setup-master.sh
@@ -5,3 +5,4 @@ echo "Master IP address : $masterIP"
 versionMajor=`kubectl version --client | awk '{print $3}' | cut -d ':' -f 2`
 versionMinor=`kubectl version --client | awk '{print $4}' | cut -d ':' -f 2`
 version="$versionMajor.$versionMinor"
+echo "$version"
