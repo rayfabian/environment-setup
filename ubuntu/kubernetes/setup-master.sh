dc0115c9a2297cf33d19da3e69996ffac67e062c
Ray Fabian <rayfabian@Reys-MacBook-Pro.local>
2018-06-17 23:41:53 +1200

    update readme

--- ubuntu/kubernetes/setup-master.sh
+++ ubuntu/kubernetes/setup-master.sh
@@ -2,3 +2,6 @@
 #get IP address
 masterIP=`ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`
 echo "Master IP address : $masterIP"
+versionMajor=`kubectl version --client | awk '{print $3}' | cut -d ':' -f 2`
+versionMinor=`kubectl version --client | awk '{print $4}' | cut -d ':' -f 2`
+version="$versionMajor.$versionMinor"
